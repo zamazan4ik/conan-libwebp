789f4ec40185c5af26a18ec06061808e38236ab6
Alexander Zaitsev <zamazan4ik@tut.by>
2017-11-01 14:03:22 +0300

    Fix mac builds

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,14 +7,14 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
     brew outdated pyenv || brew upgrade pyenv
     brew install pyenv-virtualenv
-    brew upgrade cmake || true
+    brew install cmake || true
 
     if which pyenv > /dev/null; then
 	eval "$(pyenv init -)"
     fi
 
-    pyenv install $PYTHON_VERSION
-    pyenv virtualenv $PYTHON_VERSION conan
+    pyenv install 2.7.10
+    pyenv virtualenv 2.7.10 conan
     pyenv rehash
     pyenv activate conan
 fi
